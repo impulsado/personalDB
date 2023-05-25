9b11590697d230b89c18cf48d5c425c7c1545fc7
☁️ <72570835+impulsado@users.noreply.github.com>
2023-05-25 20:18:17 +0200

    (ADD) New categories in _databaseHelpers map

--- lib/database/database_helper_factory.dart
+++ lib/database/database_helper_factory.dart
@@ -2,12 +2,18 @@ import 'package:personaldb/database/database_helper_common.dart';
 import 'package:personaldb/database/database_helper_cooking.dart';
 import 'package:personaldb/database/database_helper_health.dart';
 import 'package:personaldb/database/database_helper_ideas.dart';
+import 'package:personaldb/database/database_helper_others.dart';
+import 'package:personaldb/database/database_helper_personal.dart';
+import 'package:personaldb/database/database_helper_restaurant.dart';
 
 class DatabaseHelperFactory {
   static final Map<String, DatabaseHelperCommon> _databaseHelpers = {
     "Ideas": IdeasDatabaseHelper(),
     "Cooking": CookingDatabaseHelper(),
     "Health": HealthDatabaseHelper(),
+    "Personal": PersonalDatabaseHelper(),
+    "Restaurant": RestaurantDatabaseHelper(),
+    "Others": OthersDatabaseHelper(),
   };
 
   static DatabaseHelperCommon getDatabaseHelper(String category) {
