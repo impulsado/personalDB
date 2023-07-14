3f543c9ac24338dfa99a6626d48cc9ee36f7f834
☁️ <72570835+impulsado@users.noreply.github.com>
2023-07-14 14:47:07 +0200

    (MOD) Contacts Comming Soon!

--- lib/contacts/contacts.dart
+++ lib/contacts/contacts.dart
@@ -3,12 +3,24 @@ import 'package:flutter/material.dart';
 class Contacts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        child: Text('COMMING SOON!'),
-        onPressed: () {
-          // COMMING SOON!
-        },
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.lock,
+            color: Colors.grey,
+            size: 100.0,
+          ),
+          SizedBox(height: 10,),
+          Text(
+            'COMING SOON!',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 15,
+            ),
+          ),
+        ],
       ),
     );
   }
