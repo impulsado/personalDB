4fbb3815ffcb57b9f79bfc95a34f4472427c71d4
impulsado <impulsado@protonmail.com>
2023-07-30 21:23:38 +0200

    (MOD) Add "title" & "hint" properties.

--- lib/widgets/cupertino_date_picker.dart
+++ lib/widgets/cupertino_date_picker.dart
@@ -5,23 +5,27 @@ import 'package:personaldb/widgets/input_field.dart';
 class CupertinoDatePickerField extends StatefulWidget {
   final TextEditingController controller;
   final DateFormat dateFormatter;
+  final String title;
+  final String hint;
 
-  const CupertinoDatePickerField({super.key,
+  const CupertinoDatePickerField({
+    Key? key,
     required this.controller,
     required this.dateFormatter,
-  });
+    this.title = "Date",
+    this.hint = "Select date.",
+  }) : super(key: key);
 
   @override
   // ignore: library_private_types_in_public_api
-  _CupertinoDatePickerFieldState createState() =>
-      _CupertinoDatePickerFieldState();
+  _CupertinoDatePickerFieldState createState() => _CupertinoDatePickerFieldState();
 }
 
 class _CupertinoDatePickerFieldState extends State<CupertinoDatePickerField> {
   DateTime? selectedDate;
 
   _selectDate(BuildContext context) async {
-    selectedDate = widget.controller.text.isEmpty || widget.controller.text == "Select Date"
+    selectedDate = widget.controller.text.isEmpty || widget.controller.text == widget.hint
         ? DateTime.now()
         : widget.dateFormatter.parse(widget.controller.text);
 
@@ -44,7 +48,7 @@ class _CupertinoDatePickerFieldState extends State<CupertinoDatePickerField> {
                 ),
               ),
               CupertinoButton(
-                child: Text("OK"),
+                child: const Text("OK"),
                 onPressed: () {
                   if (selectedDate != null) {
                     widget.controller.text =
@@ -66,8 +70,8 @@ class _CupertinoDatePickerFieldState extends State<CupertinoDatePickerField> {
       onTap: () => _selectDate(context),
       child: AbsorbPointer(
         child: MyInputField(
-          title: "Date",
-          hint: "Select date.",
+          title: widget.title,
+          hint: widget.hint,
           controller: widget.controller,
         ),
       ),
